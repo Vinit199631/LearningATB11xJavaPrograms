e786c732969677a9098ab095673074158d5e3ddf
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 23:29:25 +0530

    10 March Lab0010_PrintVsPrintln.java

---
+++ src/ex_01_Java_Basic/Lab010_PrintVsPrintln.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab010_PrintVsPrintln {
+    public static void main(String[] args) {
+         System.out.println("Hello");
+         System.out.print("World");
+
+    }
+}
