72e4a1431ae6d71fc51f13f15b17c6c348906710
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 23:19:38 +0530

    10 March Lab008_Common_Math_Operations.java

---
+++ src/ex_01_Java_Basic/Lab008_Common_Math_Operations.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Common_Math_Operations {
+    public static void main(String[] args) {
+        System.out.println(2+1);
+        System.out.println(3*4);
+        System.out.println(5-4);
+        System.out.println(3/2);
+//        println -> Integer part -> no decimal
+    }
+}
