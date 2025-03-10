f5c9706dd25b56a1cfd9a7c4e88647c4a97c1d88
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 22:46:21 +0530

    10 March Lab006_ShortCuts.java

---
+++ src/ex_01_Java_Basic/Lab006_ShortCuts.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab006_ShortCuts {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+//        Using Ctrl + D
+
+    }
+}
