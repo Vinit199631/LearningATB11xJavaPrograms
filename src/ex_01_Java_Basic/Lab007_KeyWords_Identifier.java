30302a169a2601c3fdfe752c0fe8099d2d4bd8d2
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 23:10:35 +0530

    10 March Lab007_KeyWords_Identifier.java

---
+++ src/ex_01_Java_Basic/Lab007_KeyWords_Identifier.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab007_KeyWords_Identifier {
+    public static void main(String[] args) {
+        System.out.println("$RichiRich_007");
+        System.out.print("Identifier");
+    }
+}
