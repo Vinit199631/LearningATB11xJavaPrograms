f065b47eabcae8887980258832c513e6f785ccd8
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 22:38:30 +0530

    10 March Lab005_MultiComments.java

---
+++ src/ex_01_Java_Basic/Lab005_MultiComments.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab005_MultiComments {
+    public static void main(String[] args) {
+  /* Learning to add multi comments
+  *Automation Testing
+  * With Java
+  * Intellij IDEA
+   */
+        System.out.println("MultiComments");
+    }
+}
