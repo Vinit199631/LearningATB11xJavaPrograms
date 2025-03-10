e1bede94f7e1b464ba780b488dc8f7a496b2b914
vbhosale <Stout.vinit31@gmail.com>
2025-03-10 22:30:14 +0530

    10 March Lab004_Comments.java

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+//        Just to saw the comments
+//        Having Fun while learning
+        System.out.println("StoutVinit");
+    }
+}
