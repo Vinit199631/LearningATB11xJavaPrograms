f9bfa7038ff1b41f22ee735848d51ba802de2ea8
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 11:06:02 +0530

    11 March Lab0019_Numeric_Data_Types.java

---
+++ src/ex_02_Java_Basicspart2/Lab019_Numeric_Data_Types.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab019_Numeric_Data_Types {
+    public static void main(String[] args) {
+        //byte b = 128; //-128 to 127
+      long phone_number = 9878671521l;
+      long phone_number2 = 9878671521L;
+
+    }
+}
