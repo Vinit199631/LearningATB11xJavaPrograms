df63a96a18370c2339a4970f2109c7f5ff32678b
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 10:39:16 +0530

    11 March Lab0015_Variables_5.java

---
+++ src/ex_02_Java_Basicspart2/Lab015_Variables_5.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab015_Variables_5 {
+    public static void main(String[] args) {
+//        int _ = 65;
+        int $ = 65;
+        int this_is_a_very_long_name_in_the_class_hello_$_vinit = 89;
+
+    }
+}
