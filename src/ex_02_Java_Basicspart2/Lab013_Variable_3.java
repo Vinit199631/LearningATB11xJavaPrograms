6f94d5d1c5b199cbf63d00ddafa57d566d369885
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 10:31:53 +0530

    11 March Lab0013_Variables_3.java

---
+++ src/ex_02_Java_Basicspart2/Lab013_Variable_3.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab013_Variable_3 {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65+1;
+        System.out.println(a);
+
+    }
+}
