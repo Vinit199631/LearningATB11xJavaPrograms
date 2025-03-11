cf2ab7f9e43ffeaffabfdbd89488118bb1d89957
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 10:52:36 +0530

    11 March Lab0017_Interview_QnA2.java

---
+++ src/ex_02_Java_Basicspart2/Lab017_Interview_QnA2.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab017_Interview_QnA2 {
+    public static void main(String[] args) {
+//        int enum = 10; -> is not allow because enum is java keyword
+        int ENUM = 10;
+        int a_b = 89;
+//        int vinit bhosale = 100; -> is not valid because no space is allow in variable_name
+        int vinit$bhosale = 89;
+        int Lab013 = 89;
+    }
+}
