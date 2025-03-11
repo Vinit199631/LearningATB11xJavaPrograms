5e92bde2c2186ad861e4cfdfaf182f8ded0d1974
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 11:16:46 +0530

    11 March Lab020_Byte.java

---
+++ src/ex_02_Java_Basicspart2/Lab20_Byte.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab20_Byte {
+    public static void main(String[] args) {
+        byte age = 78;//0001010- 8 Bits can max 127 value
+//        character is also a integer
+    }
+}
