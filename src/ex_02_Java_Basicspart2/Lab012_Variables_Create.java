6bf64a2e1cddd7a0cc594188249f34654b8c43d8
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 10:27:53 +0530

    11 March Lab0012_Variables_Create.java

---
+++ src/ex_02_Java_Basicspart2/Lab012_Variables_Create.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab012_Variables_Create {
+    public static void main(String[] args) {
+//        data_type variable_name = variable = value
+        byte b = 10;
+//        byte-> data_type - type of container - store only integer value but limited to minimum and maximum size.
+//        -128 to 127
+//        b-> variable_name-> identifier
+//        = -> assignment - assign the value to the b
+//        10 -> literal -> integer value
+
+    }
+}
