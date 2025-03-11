d0de31112e37201ee414af3a30f27dba7d7e4b62
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 11:39:45 +0530

    11 March Lab023_Local_Variable.java

---
+++ src/ex_02_Java_Basicspart2/Lab23_Local_Variable.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab23_Local_Variable {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 10;
+        short s = 10;
+        long l = 98736487463l;
+        float f = 3.14f;
+        double d = 67.876567;
+        boolean b1 = true;
+        boolean b2 = false;
+        char c ='A';
+    }
+}
