d1de9ea81f204823c960ecb51a74680e8f06e87a
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 10:58:40 +0530

    11 March Lab0018_Boolean.java

---
+++ src/ex_02_Java_Basicspart2/Lab018_Boolean.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab018_Boolean {
+    public static void main(String[] args) {
+        boolean is_vinit_married = false;
+        boolean is_vinit_single = true;
+        boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+    }
+}
