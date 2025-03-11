068b9e37ae71dd4501021a4cfda303891a007901
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 11:34:55 +0530

    11 March Lab022_Constants.java

---
+++ src/ex_02_Java_Basicspart2/Lab22_Constants.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab22_Constants {
+    public static void main(String[] args) {
+        final int a = 12;
+//        a = 12;// a = 12; not possible to change the value if your put final
+
+        final float PI = 3.14f;
+//        PI = 90;-> No one can change the PI value , PI =90; is not possible
+    }
+}
