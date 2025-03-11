01b7dd2fe1c6d1c142992ee99de675739409eb2e
vbhosale <Stout.vinit31@gmail.com>
2025-03-11 11:46:06 +0530

    11 March Lab024_Multiple_variables.java

---
+++ src/ex_02_Java_Basicspart2/Lab24_Multiple_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basicspart2;
+
+public class Lab24_Multiple_Variables {
+    public static void main(String[] args) {
+        int a = 10 , b = 20 , c = 30;
+        int _ = 10;
+//        System.out.println(_);
+    }
+}
