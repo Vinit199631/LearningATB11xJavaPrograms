0e9fa8b0deee282771f81dea665daebc51ceda40
vbhosale <Stout.vinit31@gmail.com>
2025-03-12 11:31:24 +0530

    11 March Task001_Table_9.java

---
+++ src/TASKS/Task001_Table_9.java
@@ -0,0 +1,20 @@
+package TASKS;
+
+public class Task001_Table_9 {
+    public static void main(String[] args) {
+        int a = 9;
+        System.out.printf("%d*1=%d\n",a,a);
+        System.out.printf("%d*1=%d\n",a,a*2);
+        System.out.printf("%d*1=%d\n",a,a*3);
+        System.out.printf("%d*1=%d\n",a,a*4);
+        System.out.printf("%d*1=%d\n",a,a*5);
+        System.out.printf("%d*1=%d\n",a,a*6);
+        System.out.printf("%d*1=%d\n",a,a*7);
+        System.out.printf("%d*1=%d\n",a,a*8);
+        System.out.printf("%d*1=%d\n",a,a*9);
+        System.out.printf("%d*1=%d\n",a,a*10);
+
+
+    }
+}
+
