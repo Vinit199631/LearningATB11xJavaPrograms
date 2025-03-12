b2fc9dbbeb9130a1ddd1a3b8bb0d0801514c70b2
vbhosale <Stout.vinit31@gmail.com>
2025-03-12 11:28:06 +0530

    11 March printF_Exe01.java

--- src/ex_02_Java_Basicspart2/printF_Exe01.java
+++ src/ex_02_Java_Basicspart2/printF_Exe01.java
@@ -1,48 +1,20 @@
 package ex_02_Java_Basicspart2;
 
 public class printF_Exe01 {
-//    Print the table of 9 by using the printf command
-//    int a = 10;
-//System.out.printf("You variable is %d",a);
-//int table = 9
-//
-//9×1 = 9
-//9×2 = 18
-//
-//…
-//
-//9×10 = 90
+
 public static void main(String[] args) {
-    final int a = 9;
-        int b = 1;
-        int c = 2;
-        int d = 3;
-        int e = 4;
-        int f = 5;
-        int g = 6;
-        int h = 7;
-        int i = 8;
-        int j = 9;
-        int k = 10;
-    System.out.printf("%d * %d = %d",a,b,a*b);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,c,a*c);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,d,a*d);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,e,a*e);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,f,a*f);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,g,a*g);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,h,a*h);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,i,a*i);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,j,a*j);
-    System.out.println("  ");
-    System.out.printf("%d * %d = %d",a,k,a*k);
+     int a = 9;
+    System.out.printf("%d*1=%d\n",a,a*1);
+    System.out.printf("%d*1=%d\n",a,a*2);
+    System.out.printf("%d*1=%d\n",a,a*3);
+    System.out.printf("%d*1=%d\n",a,a*4);
+    System.out.printf("%d*1=%d\n",a,a*5);
+    System.out.printf("%d*1=%d\n",a,a*6);
+    System.out.printf("%d*1=%d\n",a,a*7);
+    System.out.printf("%d*1=%d\n",a,a*8);
+    System.out.printf("%d*1=%d\n",a,a*9);
+    System.out.printf("%d*1=%d\n",a,a*10);
+
 
 }
 }
