d935ec231c9c2e73b2a9265938312bd2fe329a7f
vbhosale <Stout.vinit31@gmail.com>
2025-03-14 20:35:15 +0530

    TASKS004_IC_OP.java

---
+++ src/TASKS/TASK004_IC_OP.java
@@ -0,0 +1,10 @@
+package TASKS;
+
+public class TASK004_IC_OP {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        System.out.println(a);
+        //34 & 13--> Ans
+    }
+}
