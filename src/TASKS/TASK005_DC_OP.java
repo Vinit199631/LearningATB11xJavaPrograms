9e78f1917f194a82341c0219337bf646311516dc
vbhosale <Stout.vinit31@gmail.com>
2025-03-14 20:35:53 +0530

    TASKS005_DC_OP.java

---
+++ src/TASKS/TASK005_DC_OP.java
@@ -0,0 +1,11 @@
+package TASKS;
+
+public class TASK005_DC_OP {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println(--a + a++ + a--  );
+        System.out.println(a);
+        //58 & 19--> Ans
+    }
+
+}
