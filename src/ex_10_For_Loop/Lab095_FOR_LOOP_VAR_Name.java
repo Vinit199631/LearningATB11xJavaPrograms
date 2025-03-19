2c4264c70bb0fddfaef9d3f71f502c269728da84
vbhosale <Stout.vinit31@gmail.com>
2025-03-19 19:48:28 +0530

    ex_10_For_Loop.java

---
+++ src/ex_10_For_Loop/Lab095_FOR_LOOP_VAR_Name.java
@@ -0,0 +1,10 @@
+package ex_10_For_Loop;
+
+public class Lab095_FOR_LOOP_VAR_Name {
+    public static void main(String[] args) {
+        for (int _1 = 0; _1 <= 10; _1++) { // 11 times
+            System.out.println(_1); // 0, to 10
+        }
+
+    }
+}
