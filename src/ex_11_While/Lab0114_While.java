139e1cc7fa2f3d91971045223f958c104b806d56
vbhosale <Stout.vinit31@gmail.com>
2025-03-20 21:19:42 +0530

    ex_11_While.java

---
+++ src/ex_11_While/Lab0114_While.java
@@ -0,0 +1,11 @@
+package ex_11_While;
+
+public class Lab0114_While {
+    public static void main(String[] args) {
+
+        while (true){
+            System.out.println("Vinit Bhosale");
+            break;
+        }
+    }
+}
