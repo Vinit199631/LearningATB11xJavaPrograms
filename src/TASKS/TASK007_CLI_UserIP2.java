27337ae8a1e02b4683cce8c3dbb20747497a256d
vbhosale <Stout.vinit31@gmail.com>
2025-03-20 17:41:38 +0530

    ex_10_For_Loop.java

--- src/TASKS/TASK007_CLI_UserIP2.java
+++ src/TASKS/TASK007_CLI_UserIP2.java
@@ -1,14 +1,37 @@
 package TASKS;
 
+import java.util.Scanner;
+
 public class TASK007_CLI_UserIP2 {
     // Take a user input 2 numbers from the arguments
     // and calculate the maximum in between with ternary operator.
     public static void main(String[] args) {
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
-        System.out.println(Math.max(num1,num2));
-        String result = num1 > num2?"num1 is max" : "num2 is max" ;
-        System.out.print(result);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Your Name");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter Your Age");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter Your Salary");
+        double Salary = scanner.nextDouble();
+
+        System.out.println("\n----user Information-----");
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+        System.out.println("Salary: "+ Salary);
+
+        scanner.close();
+
+
+
+
+      //  int num1 = Integer.parseInt(args[0]);
+        //int num2 = Integer.parseInt(args[1]);
+        //System.out.println(Math.max(num1,num2));
+        //String result = num1 > num2?"num1 is max" : "num2 is max" ;
+        //System.out.print(result);
     }
 
 }
---
+++ src/ex_10_For_Loop/Lab0101_FoorLoop_2.java
@@ -0,0 +1,14 @@
+package ex_10_For_Loop;
+
+public class Lab0101_FoorLoop_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+
+        }
+        for (float f = 0.0f; f <10.67 ; f++) {
+            System.out.println("Hi.Float--> "+f);//Not advisable only use int
+            
+        }
+    }
+}
