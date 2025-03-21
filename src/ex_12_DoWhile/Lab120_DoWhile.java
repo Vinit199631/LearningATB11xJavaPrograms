26b00d537cbf46c50e6e01564529bba98210c581
vbhosale <Stout.vinit31@gmail.com>
2025-03-22 00:47:19 +0530

    ex_12_DoWhile.java

---
+++ src/ex_12_DoWhile/Lab120_DoWhile.java
@@ -0,0 +1,16 @@
+package ex_12_DoWhile;
+
+public class Lab120_DoWhile {
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println("This is a body which will execue one time");
+            System.out.println(a);
+            a++;
+        } while(a<10);
+
+
+
+
+    }
+}
---
+++ src/ex_12_DoWhile/Lab122_DoWhile_REAL.java
@@ -0,0 +1,20 @@
+package ex_12_DoWhile;
+
+public class Lab122_DoWhile_REAL {
+    public static void main(String[] args) {
+        // Web Automation
+       int number = 0;   // it can be 0 to 10- Elements or students
+        do {
+            System.out.println(number);
+            //call function(number)
+            number++;
+        } while (number<0);
+
+        }
+        }
+
+
+
+
+
+
