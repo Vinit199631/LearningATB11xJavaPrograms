27b00ecc5e82e9bc2721f820d46fd8455a2c59cb
vbhosale <Stout.vinit31@gmail.com>
2025-04-08 00:52:47 +0530

    ex_18_OOPs_Inheritance.java

---
+++ src/ex_18_OOPs_Inheritance/hybrid/Lab180_Hybrid.java
@@ -0,0 +1,13 @@
+package ex_18_OOPs_Inheritance.hybrid;
+
+public class Lab180_Hybrid {
+    // Not possible by using classes
+
+}
+
+class GF{}
+
+class Father extends GF{}
+class Mother extends GF{}
+// class Son extends Mother, Father{}- Not possible using class , using interfaces it is possible
+
