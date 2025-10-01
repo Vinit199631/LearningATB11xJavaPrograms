1db14197ba7bc8b9e12f8d70e1fd09f3a69612ce
Vinit199631 <stout.vinit31@gmail.com>
2025-10-01 20:34:35 +0530

    Practice_JavaPrograms.iml

---
+++ src/practice_daily/Oppsssss.java
@@ -0,0 +1,32 @@
+package practice_daily;
+
+public class Oppsssss {
+    public static void main(String[] args) {
+        Person amit = new Person();
+    }
+
+}
+
+class Person{
+    // Attribute , Properties , Data Mem , Instance var
+String name;
+String phone ;
+String colour_eyes ;
+int legs;
+double salary;
+byte age;
+double weight;
+boolean isMale;
+    // Behaviour
+    void sleep(){
+        System.out.println("I am sleeping");
+    }
+
+    String greet(String name){
+        return "hello" + name;
+    }
+
+    void talk(){
+
+    }
+}
